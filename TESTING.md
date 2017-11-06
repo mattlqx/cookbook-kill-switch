780911bbcb69e4fcf78687af7ed6c8522d0cfdbb
Matt Kulka <mattlqx@users.noreply.github.com>
2017-11-06 11:12:43 -0700

    Fix normal_exit without explicit recipe in run_list (#2)
    * fix normal_exit without explicit recipe in run_list
    
    * add CONTRIBUTING and TESTING documents
    
    * add travis wait times because its so slow

---
+++ TESTING.md
@@ -0,0 +1,9 @@
+## Testing
+
+### Test Kitchen
+
+Configure the appropriate behavior in .kitchen.yml and converge an instance.
+
+### RSpec
+
+ChefSpec tests can be run by simply running `rspec`.
--- libraries/dispatcher.rb
+++ libraries/dispatcher.rb
@@ -3,7 +3,8 @@ class Chef
     # Monkeypatch for bypassing subscriber call when we throw an exception during recipe compilation to exit
     class Dispatcher < Base
       def recipe_file_load_failed(path, exception, recipe)
-        call_subscribers(:recipe_file_load_failed, path, exception, recipe) unless recipe == 'kill-switch::default'
+        call_subscribers(:recipe_file_load_failed, path, exception, recipe) \
+          unless %w[kill-switch kill-switch::default].include?(recipe)
       end
     end
   end
